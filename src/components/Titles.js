043c1d7edd847d998834560014fbb3e50156efdf
Sambhav Kaul <sambhav.kaul12@gmail.com>
2019-08-13 23:58:28 +0530

    Add weather-app repo

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Titles = () => {
+  return (
+    <div>
+      <h1 className="title-container__title">Weather Finder</h1>
+      <h3 classNmae="title-container__subtitle">
+        Find out temperature and more :)
+      </h3>
+    </div>
+  );
+};
+
+export default Titles;
